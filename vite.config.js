be06c428a7c1a0170759ad83501f245e1fb47ae5
francisco <eduabanto22@gmail.com>
2024-09-26 02:35:45 -0500

    Subiendo a la repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/clinica-dental/', // Reemplaza "nombre-del-repo" con el nombre de tu repositorio
+  base: '/clinica-dental/', // Reemplaza "nombre-del-repo" con el nombre de tu repositoriod
   plugins: [react()], 
 })
