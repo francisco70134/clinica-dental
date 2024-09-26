3839d43fdba3bd4745ce0a934f5144fedc965ffb
francisco <eduabanto22@gmail.com>
2024-09-26 02:31:02 -0500

    Configurar base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/clinica-dental/', // Reemplaza "nombre-del-repo" con el nombre de tu repositorio
+  plugins: [react()], 
 })
